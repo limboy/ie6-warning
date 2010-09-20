7d29f21dbbe5747bef9cb5f61a6ecd37ec1d9a4d
lzyy <healdream@gmail.com>
2010-09-20 21:55:27 +0800

    init

---
+++ ie6-warning.js
@@ -0,0 +1,35 @@
+function setCookie(name, value) {
+	if(name != '')
+		today = new Date();
+	expires = new Date(today.getTime() + (8 * 7 * 86400000)); 
+	document.cookie = name + '=' + value + '; expires=' + expires;
+}
+
+function getCookie(name) {
+	if(name == '')
+		return('');
+
+	name_index = document.cookie.indexOf(name + '=');
+
+	if(name_index == -1)
+		return('');
+
+	cookie_value =  document.cookie.substr(name_index + name.length + 1, document.cookie.length);
+
+	end_of_cookie = cookie_value.indexOf(';');
+	if(end_of_cookie != -1)
+		cookie_value = cookie_value.substr(0, end_of_cookie);
+
+	space = cookie_value.indexOf('+');
+	while(space != -1) { 
+		cookie_value = cookie_value.substr(0, space) + ' ' + 
+		cookie_value.substr(space + 1, cookie_value.length);
+		space = cookie_value.indexOf('+');
+	}
+
+	return(cookie_value);
+}
+
+if (getCookie('warning_has_shown') != 'yes') {
+	location.href = 'http://localhost/lab/side_project/ie6-warning/warning.html?return='+location.href;
+}
